ac557fe1bd18dd6d0c7b1909ec8f78f971d19027
Breno Pereira <brenomartinsxd@gmail.coma>
2020-06-14 19:34:16 -0400

    Added: changes into message history

--- app/Controllers/Http/WhatsappController.js
+++ app/Controllers/Http/WhatsappController.js
@@ -12,55 +12,57 @@ class WhatsappController {
 
     const chat = await Chat.query().where("phone", phone).first();
 
-    if ((isnum && message.length === 8) || message.length === 6) {
-      return response.status(200).send({
-        status: true,
-        messages: [
-          "Legal, agora preciso do seu CPF. Apenas números, sem pontos ou traços.",
-        ],
-      });
-    }
+    if (chat) {
+      if ((isnum && message.length === 8) || message.length === 6) {
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Legal, agora preciso do seu CPF. Apenas números, sem pontos ou traços.",
+          ],
+        });
+      }
 
-    if (isnum && message.length === 11) {
-      return response.status(200).send({
-        status: true,
-        messages: [
-          "Isso mesmo. Agora pra finalizar, como você gostaria que eu te chamasse.",
-        ],
-      });
-    }
+      if (isnum && message.length === 11) {
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Isso mesmo. Agora pra finalizar, como você gostaria que eu te chamasse.",
+          ],
+        });
+      }
 
-    if (!isnum && message.length === 11) {
-      return response.status(200).send({
-        status: true,
-        messages: ["Digite um CPF válido, contendo somente números."],
-      });
-    }
+      if (!isnum && message.length === 11) {
+        return response.status(200).send({
+          status: true,
+          messages: ["Digite um CPF válido, contendo somente números."],
+        });
+      }
 
-    if (!user) {
-      // await Phone.create();
-      await Chat.create({
-        message: 1,
-        status: true,
-        phone,
-      });
+      if (!user) {
+        // await Phone.create();
+        await Chat.create({
+          message: 1,
+          status: true,
+          phone,
+        });
 
-      return response.status(200).send({
-        status: true,
-        messages: [
-          "Olá, me chamo Tony e sou seu assistente virtual que irá lhe ajudar. Antes de mais nada, verifiquei aqui que esse é seu primeiro contato conosco. Vou precisar de algumas informações para podermos continuar.",
-          "Digite sua data de nascimento. Apenas números (dia, mês e ano). Por exemplo: 07121980",
-        ],
-      });
-    }
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Olá, me chamo Tony e sou seu assistente virtual que irá lhe ajudar. Antes de mais nada, verifiquei aqui que esse é seu primeiro contato conosco. Vou precisar de algumas informações para podermos continuar.",
+            "Digite sua data de nascimento. Apenas números (dia, mês e ano). Por exemplo: 07121980",
+          ],
+        });
+      }
 
-    if (user.name && user.phone && user.cpf && user.birth_date) {
-      return response.status(200).send({
-        status: true,
-        messages: [
-          `Olá ${user.name}. Veja as opções que eu tenho para você e você digita o número da opção.`,
-        ],
-      });
+      if (user.name && user.phone && user.cpf && user.birth_date) {
+        return response.status(200).send({
+          status: true,
+          messages: [
+            `Olá ${user.name}. Veja as opções que eu tenho para você e você digita o número da opção.`,
+          ],
+        });
+      }
     }
 
     return response.status(200).send({
