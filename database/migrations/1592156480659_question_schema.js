9e917f972204607bfbb0e7bce9084d9c70071a8b
Breno Pereira <brenomartinsxd@gmail.coma>
2020-06-14 17:59:08 -0400

    Added: insert database migration contain questions and options with relationship

--- database/migrations/1592156480659_question_schema.js
+++ database/migrations/1592156480659_question_schema.js
@@ -1,19 +1,22 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class QuestionSchema extends Schema {
-  up () {
-    this.create('questions', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("questions", (table) => {
+      table.increments();
+      table.string("question");
+      table.string("status");
+      table.integer("weight");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('questions')
+  down() {
+    this.drop("questions");
   }
 }
 
-module.exports = QuestionSchema
+module.exports = QuestionSchema;
--- database/migrations/1592156485659_option_schema.js
+++ database/migrations/1592156485659_option_schema.js
@@ -1,19 +1,25 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class OptionSchema extends Schema {
-  up () {
-    this.create('options', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("options", (table) => {
+      table.increments();
+      table.string("option");
+      table
+        .integer("question_id")
+        .unsigned()
+        .references("id")
+        .inTable("questions");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('options')
+  down() {
+    this.drop("options");
   }
 }
 
-module.exports = OptionSchema
+module.exports = OptionSchema;
