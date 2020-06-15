b2301cb88cab3ba1d5c4d2a85dc3620e63be8216
Breno Pereira <brenomartinsxd@gmail.coma>
2020-06-14 22:32:16 -0400

    Added: insert location and quiz and questions

--- app/Controllers/Http/WhatsappController.js
+++ app/Controllers/Http/WhatsappController.js
@@ -8,8 +8,10 @@ class WhatsappController {
   async store({ response, request }) {
     const { phone, message } = request.all();
     const user = await Phone.query().where("phone", phone).first();
-    const welcome = ["Olá", "ola", "oi", "ei", "Oi", "1", "2", "Ola"];
+    const welcome = ["Olá", "ola", "oi", "ei", "Oi", "Ola"];
     const isnum = /^\d+$/.test(message);
+
+    console.log(message);
 
     const chat = await Chat.query().where("phone", phone).first();
 
@@ -161,6 +163,166 @@ class WhatsappController {
         });
       }
 
+      if (user && lastMessage.message === "quiz1") {
+        await Message.create({
+          message: `quiz2`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Você se sente estressado frequentemente?  1 - Sim / 2 - Não",
+          ],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz2") {
+        await Message.create({
+          message: `quiz3`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Você tem colesterol alto? Ou você apresenta grande quantidade de gordura no corpo? 1 - Sim / 2 - Não",
+          ],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz3") {
+        await Message.create({
+          message: `quiz4`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Você tem ou já teve alguma doença no coração? 1 - Sim / 2 - Não",
+          ],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz4") {
+        await Message.create({
+          message: `quiz5`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Você é uma pessoa ativa fisicamente ou pratica atividade física frequentemente? 1 - Sim / 2 - Não",
+          ],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz5") {
+        await Message.create({
+          message: `quiz6`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: ["Qual é o seu sexo? 1 - Masculino / 2 - Feminino"],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz6") {
+        await Message.create({
+          message: `quiz7`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: ["Você tem diabetes? 1 - Sim / 2 - Não"],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz7") {
+        await Message.create({
+          message: `quiz8`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Alguém da sua família, como pai, mãe, avós ou avôs, irmãos tiveram ou tem doença no coração? 1 - Sim / 2 - Não",
+          ],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz8") {
+        await Message.create({
+          message: `quiz9`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: ["Você fuma cigarro diariamente? 1 - Sim / 2 - Não"],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz9") {
+        await Message.create({
+          message: `quiz10`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: ["Você já teve ataque cardíaco? 1 - Sim / 2 - Não"],
+        });
+      }
+
+      if (user && lastMessage.message === "quiz10") {
+        await Message.create({
+          message: `result`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: ["Qual sua idade?"],
+        });
+      }
+
+      if (user && lastMessage.message === "result") {
+        await Message.create({
+          message: `Encontramos alterações na sua saúde que merecem uma avaliação médica mais de perto.`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Encontramos alterações na sua saúde que merecem uma avaliação médica mais de perto.",
+            "Me envie sua localização para que eu possa te indicar um posto de atendimento da CCR mais próximo. Assim você podera realizar uma avaliação médica.",
+          ],
+        });
+      }
+
+      if (message.length > 1000) {
+        await Message.create({
+          message: `Posto BR 1. São Paulo/SP`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            "Certo, esses são os endereços próximos a você.",
+            "Posto BR 1. São Paulo/SP",
+          ],
+        });
+      }
+
       if (
         user &&
         user.phone &&
@@ -189,26 +351,6 @@ class WhatsappController {
         });
       }
 
-      // if (lastMessage.message === "*Saude*1. Quiz" && !isnum) {
-      //   await Message.create({
-      //     message: `Legal ${message}. Veja as opções que eu tenho para você e você digita o número da opção.`,
-      //     chat_id: chat.id,
-      //   });
-
-      //   await Message.create({
-      //     message: "*Saude*1. Quiz",
-      //     chat_id: chat.id,
-      //   });
-
-      //   return response.status(200).send({
-      //     status: true,
-      //     messages: [
-      //       `Legal ${user.name}. Veja as opções que eu tenho para você e você digita o número da opção.`,
-      //       "*Saude*1. Quiz",
-      //     ],
-      //   });
-      // }
-
       if (lastMessage.message === "*Saude*1. Quiz" && isnum) {
         if (message === "1") {
           await Message.create({
@@ -217,10 +359,16 @@ class WhatsappController {
             chat_id: chat.id,
           });
 
+          await Message.create({
+            message: "quiz1",
+            chat_id: chat.id,
+          });
+
           return response.status(200).send({
             status: true,
             messages: [
               `Você selecionou o 1. Quiz e vamos iniciar uma avaliação para saber como estar sua saúde.`,
+              `Você tem pressão alta? 1 - Sim / 2 - Não`,
             ],
           });
         } else {
@@ -242,6 +390,31 @@ class WhatsappController {
             ],
           });
         }
+      }
+
+      if (
+        lastMessage.message ===
+          "Você selecionou o 1. Quiz e vamos iniciar uma avaliação para saber como estar sua saúde." &&
+        user &&
+        welcome.includes(message)
+      ) {
+        await Message.create({
+          message: `Legal ${user.name}. Veja as opções que eu tenho para você e você digita o número da opção.`,
+          chat_id: chat.id,
+        });
+
+        await Message.create({
+          message: `*Saude*1. Quiz`,
+          chat_id: chat.id,
+        });
+
+        return response.status(200).send({
+          status: true,
+          messages: [
+            `Legal ${user.name}. Veja as opções que eu tenho para você e você digita o número da opção.`,
+            "*Saude*1. Quiz",
+          ],
+        });
       }
     } else {
       const chat = await Chat.create({
